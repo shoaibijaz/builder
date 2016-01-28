68ffae1b428c66e890830d3911b5b15b3a504c97
Canh Ho <canhhs91@gmail.com>
2016-01-28 00:46:04 +0000

    change name

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "builder.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "electric.settings")
 
     from django.core.management import execute_from_command_line
 
